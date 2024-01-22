fe1365f83c9ae85b3934c8fe244a7a7f91f80ad1
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-22 21:59:05 +0530

    commit 22/1/24

---
+++ 02. Java Tutorial Variables and Data Types in Java Programming/first.java
@@ -0,0 +1,13 @@
+public class first {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int num1 = 4;
+        int num2 = 6;
+        int num3 = 8;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+    }
+}
---
+++ 02. Java Tutorial Variables and Data Types in Java Programming/practice.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        //1.
+        // int num1 = 4;
+        // int num2 = 6;
+        // int num3 = 8;
+        // int sum = num1 + num2 + num3;
+        // System.out.println(sum);
+
+        //2.
+        // int mark1 = 98;
+        // int mark2 = 89;
+        // int mark3 = 99;
+        // int avg = (mark1+mark2+mark3)/3;
+        // System.out.println("Percentage is:- ");
+        // System.out.println(avg);
+
+        //3.
+        // String name = "Shlok";
+        // System.out.println("Hello " + name + ", have a good day");
+
+        //4.
+        int km = 2;
+        int mile = 
+    }
+}
