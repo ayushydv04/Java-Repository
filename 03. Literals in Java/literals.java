09e777fb8fee51cc87d8efe8267cd6035ed36f20
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-23 23:24:37 +0530

    commit 23/1/24

---
+++ 03. Literals in Java/literals.java
@@ -0,0 +1,18 @@
+public class literals{
+    public static void main(String[] args) {
+        System.out.println("Literals in Java");
+        int age1 = 10;
+        short age2 = 12;
+        byte age3 = 45;
+        long age4 = 455555555;
+        double d1 = 4.66d;
+        float f1 = 5.6f;
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        System.out.println(d1);
+        System.out.println(f1);
+
+    }
+}
