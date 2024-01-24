a576a8d60a3363bb8485aa12ad76e8384dbc5c61
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-24 10:09:18 +0530

    commit 24/1/24

---
+++ 05. Operators, Types of Operators/Operator.java
@@ -0,0 +1,27 @@
+public class Operator{
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+        // int a = 4;
+        // int b = 5;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        // System.out.println(b%a);
+
+        // 2. Assignment Operator
+        // int c = 9;
+        // c *= 3;  //c = c * 9
+        // System.out.println(c);
+
+        // 3. Coparison Operator
+        // System.out.println(66>3);
+
+        // 4. Logical Operators
+        System.out.println(65>5 && 300>99);
+
+        // 5. Bitwise Operator
+        // System.out.println(2&3);  //Bitwise and
+        // System.out.println(2|3);  //Bitwise or
+
+    }
+}
