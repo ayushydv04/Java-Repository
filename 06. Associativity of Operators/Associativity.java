3167830a59fcff9d3e3d62c61382f1b3bae98c25
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-25 22:20:08 +0530

    commit 25/1/24

---
+++ 06. Associativity of Operators/Associativity.java
@@ -0,0 +1,18 @@
+public class Associativity {
+
+    public static void main(String[] args) {
+        // Precedence
+        // int a = 6*5-34/2;
+        // System.out.println(a);
+
+        // int b = 60/5-34*2;
+        // System.out.println(b);
+
+        int b = 4;
+        int c = 2;
+        int a = 10;
+        int k = b*b - (4*a*c)/(2*a);
+        System.out.println(k);
+
+    }
+}
