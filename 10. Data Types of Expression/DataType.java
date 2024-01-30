2d8c139122390c93e0e103b041a8d4156d664b42
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-30 22:51:57 +0530

    commit 30/1/24

---
+++ 10. Data Types of Expression/DataType.java
@@ -0,0 +1,25 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 8;
+        short c = 4;
+        long d = 5666666;
+        float e = 4.5f;
+        double f = 7.88d;
+        char g = 'a';
+
+        // System.out.println(a);
+        // int h = a++;
+        // System.out.println(a);
+
+        System.out.println(a);
+        // int i = ++a;
+        // System.out.println(i);
+        System.out.println(++a);
+
+        
+        
+
+    }
+}
