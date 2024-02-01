b96d9edb5d7ffce11b78a396bbc4c8dabc62fd4c
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-02 02:19:14 +0530

    commit 2/2/24

---
+++ 13. Java Conditionals If-else Statement in Java/conditional.java
@@ -0,0 +1,13 @@
+public class conditional {
+
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age>=18) {
+            System.out.println("You can Drive");
+        }
+        else{
+            System.out.println("You cannot Drive");
+        }
+    }
+}
