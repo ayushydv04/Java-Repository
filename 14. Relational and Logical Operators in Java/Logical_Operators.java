81b9ea98bf2efeb873f922dfc92642771dc66ad8
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-03 01:24:53 +0530

    commit 3/2/24

---
+++ 14. Relational and Logical Operators in Java/Logical_Operators.java
@@ -0,0 +1,34 @@
+public class Logical_Operators {
+    public static void main(String[] args) {
+        // System.out.println("Logical AND &&");
+        // boolean a = true;
+        // boolean b = false;
+        // if (a && b) {
+        //     System.out.println("Y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+
+        // System.out.println("Logical OR ||");
+        // boolean a = true;
+        // boolean b = false;
+        // if (a || b) {
+        //     System.out.println("Y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+
+        System.out.println("Logical NOT !");
+        boolean a = true;
+        boolean b = false;
+        System.out.print("Not(a) = ");
+        System.out.println(!a);
+        System.out.print("Not(b) = ");
+        System.out.println(!b);
+
+    }
+}
