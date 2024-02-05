372f1257d481bb642340e2f15aeaaf57425e299f
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-06 00:03:42 +0530

    commit 6/2/24

---
+++ 17. The do-while loop in Java/doWhile.java
@@ -0,0 +1,11 @@
+public class doWhile {
+
+    public static void main(String[] args) {
+        System.out.println("do while loop");
+        int a = 0;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a<5);
+    }
+}
