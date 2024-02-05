f5bbd85ddeab5045fd934be6ea67edd74b8260a4
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-05 23:59:26 +0530

    commit

---
+++ 16. While Loops in Java/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        System.out.println("While loop");
+        int i = 1;
+        while (i<=3) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
