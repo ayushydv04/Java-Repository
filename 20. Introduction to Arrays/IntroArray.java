e9d34500322f6230b232908158d36e3527715c6c
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-08 01:07:07 +0530

    commit 8/2/24

---
+++ 20. Introduction to Arrays/IntroArray.java
@@ -0,0 +1,18 @@
+public class IntroArray {
+
+    public static void main(String[] args) {
+        //3 types to make a array
+
+        // 1. Declaration and Memory allocation seperately
+        // int []marks;
+        // marks = new int[5];
+
+        // 2. declaration + Memory allocation
+        // int [] marks = new int[5];
+
+        // 3. Declare + initialize
+        // int []marks = {90, 87, 99, 92, 89};
+
+
+    }
+}
