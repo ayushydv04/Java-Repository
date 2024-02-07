021ffaacc2c0b2ccf1b8bdd5ee618e95490133d3
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-07 22:40:32 +0530

    commit 7/2/24

---
+++ 19. Break and Continue in Java/brecon.java
@@ -0,0 +1,88 @@
+public class brecon {
+
+    public static void main(String[] args) {
+        // System.out.println("In for loop");
+        // for(int i=0; i<5; i++){
+        //     System.out.println(i);
+        //     System.out.println("Java");
+        //     if (i==2) {
+        //         System.out.println("Ending Loop");
+        //         break;
+        //     }
+        // }
+
+        // System.out.println("While Loop");
+        // int i = 0;
+        // while (i<5) {
+        //     System.out.println(i);
+        //     System.out.println("Java");
+        //     if (i==2) {
+        //         System.out.println("Ending Loop");
+        //         break;
+                
+        //     }
+        //     i++;
+        // }
+
+
+        // System.out.println("Do While Loop");
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     System.out.println("Java");
+        //     if (i==2) {
+        //         System.out.println("Ending Loop");
+        //         break;
+                
+        //     }
+        //     i++;
+        // }while(i<5);
+        // System.out.println("Loop ends here");
+
+
+            System.out.println("*********Continue*********");
+
+        // System.out.println("In for loop");
+        // for(int i=0; i<5; i++){
+        //     if (i==2) {
+        //         System.out.println("Ending Loop");
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     System.out.println("Java");
+        // }
+
+        System.out.println("While Loop");
+        int i = 0;
+        while (i<5) {
+            i++;
+            if (i==2) {
+                System.out.println("Ending Loop");
+                // i++;
+                continue;
+            }
+            System.out.println(i);
+            System.out.println("Java");
+            // i++;
+        }
+
+
+        // System.out.println("Do While Loop");
+        // int i = 0;
+        // do {
+        //     i++;
+        //     if (i==2) {
+        //         System.out.println("Ending Loop");
+        //         // i++;
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     System.out.println("Java");
+        //     // i++;
+        // }while(i<5);
+        // System.out.println("Loop ends here");
+
+
+
+    }
+}
