bd826b936ba17546297be24ce6c42abead59d568
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-09 00:05:15 +0530

    commit 9/2/24

---
+++ 21. For Each loop in Java/ArrayMethods.java
@@ -0,0 +1,36 @@
+public class ArrayMethods {
+
+    public static void main(String[] args) {
+
+        int []marks = {100, 89, 90, 97, 93};
+
+        // Length of an array
+        // System.out.println(marks.length);
+
+        // Displaying an element
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+        // System.out.println(marks[4]);
+
+        // Printing an array using for loop
+        // for (int i = 0; i < marks.length; i++) {
+        //     System.out.println(marks[i]);
+        // }
+
+        // Printing reverse array using for loop
+        // for (int i = marks.length-1; i >= 0; i--) {
+        //     System.out.println(marks[i]);
+        // }
+
+
+        // For - Each loop
+        // for (int i : marks) {
+        //     System.out.println(i);
+        // }
+            
+
+
+    }
+}
