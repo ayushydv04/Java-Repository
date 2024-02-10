a7ae94dc3d343b0d8290cc9f99f6759b65d49eb7
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-10 21:53:22 +0530

    commit 10/2/24

---
+++ 23. Methods in Java/vdo.java
@@ -0,0 +1,15 @@
+public class vdo {
+
+    static int Calculate(int a, int b){
+        int sum = a+b;
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int x = Calculate(a,b);
+        System.out.println(x);
+    }
+} 
